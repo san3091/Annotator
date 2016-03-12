b3c145b75f35122ccd0cd2e9eeb4385e0c92f0c9
san3091 <san3091@gmail.com>
2016-03-12 17:19:03 -0500

    Added callback methods for bi-directional association

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,4 +1,34 @@
 class Relation < ActiveRecord::Base
   belongs_to :text
   belongs_to :related_text, class_name: "Text"
+
+  after_create :create_inverse unless :has_inverse?
+  after_destroy :destroy_inverses if :has_inverse?
+
+  # The relation is created when one text is shoveled into
+  # the other text's related_texts. After the Relation is
+  # instantiated, the inverse relation is created.
+  def create_inverse
+    self.class.create(inverse_relation_options)
+  end
+
+  def destroy_inverses
+    inverses.destroy_all
+  end
+
+  # The filter only runs if it is necessary
+  def has_inverse?
+    self.class.exists?(inverse_relation_options)
+  end
+
+  # finds all inverse relationships of the class calling the method
+  def inverses
+    self.class.where(inverse_relation_options)
+  end
+
+  # The inverse relationship is where the related text is the
+  # text which initiated the callback, and vice versa.
+  def inverse_relation_options
+    { related_text_id: text_id, text_id: related_text_id}
+  end
 end
