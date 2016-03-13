caf958baa0e739b0f5548efdcc581207c69daa1f
san3091 <san3091@gmail.com>
2016-03-13 17:24:08 -0400

    debugging callbacks, no inverse relation created after create

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -2,21 +2,21 @@ class Relation < ActiveRecord::Base
   belongs_to :text
   belongs_to :related_text, class_name: "Text"
 
-  after_create :create_inverse unless :has_inverse?
-  after_destroy :destroy_inverses if :has_inverse?
+  after_create :create_inverse, unless: :has_inverse?
+  after_destroy :destroy_inverses, if: :has_inverse?
 
   # The relation is created when one text is shoveled into
   # the other text's related_texts. After the Relation is
   # instantiated, the inverse relation is created.
   def create_inverse
-    self.class.create(inverse_relation_options)
+      self.class.create(inverse_relation_options)
   end
 
   def destroy_inverses
     inverses.destroy_all
   end
 
-  # The filter only runs if it is necessary
+  # Check for presence before callback
   def has_inverse?
     self.class.exists?(inverse_relation_options)
   end
@@ -29,6 +29,6 @@ class Relation < ActiveRecord::Base
   # The inverse relationship is where the related text is the
   # text which initiated the callback, and vice versa.
   def inverse_relation_options
-    { related_text_id: text_id, text_id: related_text_id}
+    { related_text_id: text_id, text_id: related_text_id }
   end
 end
